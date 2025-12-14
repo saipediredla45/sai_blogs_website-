85ad45ec509f54255eb9bf0b839e1d2fc104010e
saipediredla45 <saipediredla85@gmail.com>
2025-12-14 16:01:45 +0530

    Fix production settings for Render

--- problog/settings.py
+++ problog/settings.py
@@ -19,15 +19,19 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-x24g!d51^2bdlvu3l6$^4+_*2#pziv*on!1=vbp4)$-c(h2ayd'
 
+
+import os
+
+SECRET_KEY = os.environ.get(
+    "DJANGO_SECRET_KEY",
+    "django-insecure-local-only"
+)
+
+DEBUG = os.environ.get("DEBUG", "True") == "True"
+
+ALLOWED_HOSTS = ['.onrender.com', 'localhost', '127.0.0.1']
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ["*"]
-
-
 # Application definition
 
 INSTALLED_APPS = [
